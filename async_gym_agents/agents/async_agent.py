99957dec65ada85f696c15994294d7789eb2c02d
Jonas Peche <j_peche@wargaming.net>
2025-10-14 15:33:25 +0200

    added better episode buffer overflow handling

--- async_gym_agents/agents/async_agent.py
+++ async_gym_agents/agents/async_agent.py
@@ -1,14 +1,17 @@
-from typing import Type
+from typing import Type, TypeVar, Union
 
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
 
+from async_gym_agents.agents.injector import AsyncAgentInjector
 from async_gym_agents.agents.off_policy_injector import OffPolicyAlgorithmInjector
 from async_gym_agents.agents.on_policy_injector import OnPolicyAlgorithmInjector
 
+T = TypeVar("T", bound=BaseAlgorithm)
 
-def get_injected_agent(clazz: Type[BaseAlgorithm]):
+
+def get_injected_agent(clazz: Type[T]) -> Union[Type[T], Type[AsyncAgentInjector]]:
     if issubclass(clazz, OnPolicyAlgorithm):
 
         class AsyncAgent(OnPolicyAlgorithmInjector, clazz):
--- async_gym_agents/agents/injector.py
+++ async_gym_agents/agents/injector.py
@@ -1,3 +1,4 @@
+import logging
 import queue
 import threading
 from queue import Queue
@@ -5,9 +6,16 @@ from typing import List
 
 from async_gym_agents.envs.multi_env import IndexableMultiEnv
 
+logger = logging.getLogger("async_gym_agents")
+
 
 class AsyncAgentInjector:
-    def __init__(self, max_steps_in_buffer: int = 8, skip_truncated: bool = False):
+    def __init__(
+        self,
+        max_steps_in_buffer: int,
+        skip_truncated: bool = False,
+        timeout: float = 1.0,
+    ):
         self._buffer_utilization = 0.0
         self._buffer_emptiness = 0.0
         self._buffer_stat_count = 0
@@ -17,11 +25,13 @@ class AsyncAgentInjector:
         self.threads = []
 
         self.total_episodes = 0
-        self.skipped_episodes = 0
+        self.discarded_episodes = 0
         self.skip_truncated = skip_truncated
+        self.timeout = timeout
 
         # The larger the queue, the less wait times, but the more outdated the policies training data is
         self.queue = Queue(max_steps_in_buffer)
+        self.transition_queue = Queue()
         self.episode_lock = threading.Lock()
 
         # The policy itself is rarely thread-safe
@@ -56,10 +66,13 @@ class AsyncAgentInjector:
             self.threads[index].start()
 
     def fetch_transition(self):
-        self._buffer_utilization += self.queue.qsize()
-        self._buffer_emptiness += 1 if self.queue.empty() else 0
-        self._buffer_stat_count += 1
-        return self.queue.get()
+        while self.transition_queue.empty():
+            self._buffer_utilization += self.queue.qsize()
+            self._buffer_emptiness += 1 if self.queue.empty() else 0
+            self._buffer_stat_count += 1
+            for t in self.queue.get():
+                self.transition_queue.put(t)
+        return self.transition_queue.get()
 
     @property
     def buffer_utilization(self) -> float:
@@ -78,40 +91,37 @@ class AsyncAgentInjector:
         )
 
     @property
-    def truncated_episodes_fraction(self) -> float:
+    def discarded_episodes_fraction(self) -> float:
         return (
             0
             if self.total_episodes == 0
-            else self.skipped_episodes / self.total_episodes
+            else self.discarded_episodes / self.total_episodes
         )
 
     def _episode_generator(self, index: int):
         raise NotImplementedError()
 
-    def _collector_loop(
-        self,
-        index: int,
-    ):
+    def _collector_loop(self, index: int):
         """
-        Batch-inserts transitions whenever a episode is done.
+        Batch-inserts transitions whenever an episode is done.
         """
         for episode in self._episode_generator(index):
             # Keeps track of truncated episodes and optionally removes them
             self.total_episodes += 1
-            if episode[-1].infos[0]["TimeLimit.truncated"]:
-                self.skipped_episodes += 1
-                if self.skip_truncated:
-                    continue
+            if episode[-1].infos[0]["TimeLimit.truncated"] and self.skip_truncated:
+                self.discarded_episodes += 1
+                logger.info("Dropped episode due to truncation")
+                continue
 
             # Feeds the episodes into the queue
             with self.episode_lock:
-                for transition in episode:
-                    while self.running:
-                        try:
-                            self.queue.put(transition, block=True, timeout=1)
-                            break
-                        except queue.Full:
-                            pass
+                try:
+                    self.queue.put(episode, block=True, timeout=self.timeout)
+                except queue.Full:
+                    self.queue.get(block=False)
+                    self.queue.put(episode, block=False)
+                    self.discarded_episodes += 1
+                    logger.info("Dropped episode due to buffer full")
 
     def shutdown(self):
         """
